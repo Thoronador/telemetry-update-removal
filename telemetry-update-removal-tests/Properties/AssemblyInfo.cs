f359d7af70971476b7c20ff66f7e0d37c86c8366
Thoronador <thoronador@users.sourceforge.net>
2015-09-20 22:22:44 +0200

    add project for NUnit tests of telemetry-update-removal

---
+++ telemetry-update-removal-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("telemetry-update-removal-tests")]
+[assembly: AssemblyDescription("Tests for telemetry-update-removal application.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("telemetry-update-removal-tests")]
+[assembly: AssemblyCopyright("Copyright © 2015 Thoronador")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("764d3b2c-36cb-41b1-b8d0-9f5e93841b71")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
---
+++ telemetry-update-removal-tests/TestsUpdates.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace telemetry_update_removal_tests
+{
+    /// <summary>
+    /// Class for tests of functions in telemetry_update_removal.Updates.
+    /// </summary>
+    [TestFixture]
+    public class TestsUpdates
+    {
+        /// <summary>
+        /// checks whether Updates.listInstalledUpdates() lists some updates
+        /// </summary>
+        [Test]
+        public void Test_listInstalledUpdates()
+        {
+            List<telemetry_update_removal.Updates.UpdateOpInfo> list
+                = telemetry_update_removal.Updates.listInstalledUpdates();
+            //list should not be null
+            Assert.IsNotNull(list);
+            //list should contain some elements
+            Assert.IsNotEmpty(list, "List of installed updates is empty!");
+        }
+
+
+        /// <summary>
+        /// checks whether Updates.listUpdateHistory() list some updates
+        /// </summary>
+        [Test]
+        public void Test_listUpdateHistory()
+        {
+            List<telemetry_update_removal.Updates.UpdateOpInfo> list
+                = telemetry_update_removal.Updates.listUpdateHistory();
+            //list should not be null
+            Assert.IsNotNull(list);
+            //list should contain some elements
+            Assert.IsNotEmpty(list, "Update history is empty!");
+        }
+        
+
+        /// <summary>
+        /// checks whether Updates.OperationResultCodeToString() returns the
+        /// exprected values
+        /// </summary>
+        [Test]
+        public void Test_OperationResultCodeToString()
+        {
+            Assert.AreEqual("Aborted",
+                telemetry_update_removal.Updates.OperationResultCodeToString(
+                WUApiLib.OperationResultCode.orcAborted));
+            Assert.AreEqual("Failed",
+                telemetry_update_removal.Updates.OperationResultCodeToString(
+                WUApiLib.OperationResultCode.orcFailed));
+            Assert.AreEqual("In progress",
+                telemetry_update_removal.Updates.OperationResultCodeToString(
+                WUApiLib.OperationResultCode.orcInProgress));
+            Assert.AreEqual("Not started",
+                telemetry_update_removal.Updates.OperationResultCodeToString(
+                WUApiLib.OperationResultCode.orcNotStarted));
+            Assert.AreEqual("Succeeded",
+                telemetry_update_removal.Updates.OperationResultCodeToString(
+                WUApiLib.OperationResultCode.orcSucceeded));
+            Assert.AreEqual("Succeeded with errors",
+                telemetry_update_removal.Updates.OperationResultCodeToString(
+                WUApiLib.OperationResultCode.orcSucceededWithErrors));
+        }
+
+
+        /// <summary>
+        /// checks whether Updates.UpdateOperationToString() returns the
+        /// expected string values
+        /// </summary>
+        [Test]
+        public void TestFault()
+        {
+            Assert.AreEqual("Installation",
+                telemetry_update_removal.Updates.UpdateOperationToString(
+                WUApiLib.UpdateOperation.uoInstallation));
+            Assert.AreEqual("Uninstallation",
+                telemetry_update_removal.Updates.UpdateOperationToString(
+                WUApiLib.UpdateOperation.uoUninstallation));
+        }
+    } //class
+} //namespace
